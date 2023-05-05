c082f1e22b3be2502f12e0fb20709bdf00a77897
khalelelel <76054584+khalelelel@users.noreply.github.com>
2023-05-05 09:17:34 +0000

    topnav contents

---
+++ orenjiweb/src/Components/Topnav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Topnav() {
+  return (
+    <>
+        <div className='topnav'>
+            <div className='logoname'>Lucilva</div>
+        </div>
+    </>
+  )
+}
+
+export default Topnav
